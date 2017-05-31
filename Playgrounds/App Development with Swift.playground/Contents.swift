8a8b383ebcedc06ded78d4ad314fc1ee5b22e5fd
Sergey Tolmachev <tetron@gmail.com>
2017-05-31 12:10:15 +0300

    Reorange directories

---
+++ Playgrounds/App Development with Swift.playground/Contents.swift
@@ -0,0 +1,22 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+
+let name = "John"
+print(name)
+
+var age = 29
+age = 31
+print(age)
+
+let defaultScore = 100
+var playerOneScore = defaultScore
+var playerTwoScore = defaultScore
+print(playerOneScore)
+print(playerTwoScore)
+
+playerOneScore = 200
+print(playerOneScore)
