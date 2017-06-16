740beb667991ceb962f19e08b34ed39eefa4036e
Sergey Tolmachev <tetron@gmail.com>
2017-06-16 19:07:56 +0300

    Doing Lab—Optionals.playground

--- 3 - Navigation and Workflows/1 - Optionals/lab/Lab - Optionals.playground/Pages/1. Exercise - Optionals.xcplaygroundpage/Contents.swift
+++ 3 - Navigation and Workflows/1 - Optionals/lab/Lab - Optionals.playground/Pages/1. Exercise - Optionals.xcplaygroundpage/Contents.swift
@@ -5,7 +5,9 @@
  
  Declare a constant `userInputAge` of type `String` and assign it "34e" to simulate a typo while typing age. Then declare a constant `userAge` of type `Int` and use the `Int` initializer and pass in `userInputAge`. What error do you get?
 */
-
+let userInputAge: String = "34"
+let userAge: Int? = Int(userInputAge)
+print(userAge)
 
 /*:
  Go back and change the type of `userAge` to `Int?`, and print the value of `userAge`. Why is `userAge`'s value `nil`? Provide your answer in a comment or print statement below.
@@ -17,11 +19,13 @@
  
  Print `userAge` again, but this time unwrap `userAge` using the bang operator.
  */
-
+print(userAge!)
 
 /*:
  Now use optional binding to unwrap `userAge`. If `userAge` has a value, print it to the console.
  */
-
+if let userAgeUnwrapped = userAge {
+    print(userAgeUnwrapped)
+}
 
 //: page 1 of 6  |  [Next: App Exercise - Finding a Heart Rate](@next)
